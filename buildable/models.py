9c8b55ca7eb2b880d9f431a9f355954394d5de0f
Albarra Naufala <albarraerdanto@gmail.com>
2025-05-15 09:51:33 +0700

    add settings models for logo, email, contact, and navigation configurations

--- buildable/models.py
+++ buildable/models.py
@@ -1,8 +1,14 @@
+from django.db import models
+
 from wagtail.models import Page
 from wagtail.fields import StreamField
 from wagtail import blocks
 from wagtail.images.blocks import ImageChooserBlock
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from wagtail.contrib.settings.models import (
+    BaseSiteSetting,
+    register_setting,
+)
 
 
 class BuildablePage(Page):
@@ -110,3 +116,134 @@ class BuildablePage(Page):
     content_panels = Page.content_panels + [
         FieldPanel("body"),
     ]
+
+
+@register_setting
+class LogoSettings(BaseSiteSetting):
+    logo_for_light_background = models.ForeignKey(
+        "wagtailimages.Image",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="+",
+    )
+    logo_for_dark_background = models.ForeignKey(
+        "wagtailimages.Image",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="+",
+    )
+
+
+@register_setting
+class EmailSettings(BaseSiteSetting):
+    email_host = models.CharField(max_length=255)
+    email_port = models.IntegerField()
+    email_host_user = models.CharField(max_length=255)
+    email_host_password = models.CharField(max_length=255)
+    email_use_tls = models.BooleanField(default=False)
+    email_use_ssl = models.BooleanField(default=False)
+
+
+@register_setting
+class ContactSettings(BaseSiteSetting):
+    send_email_to_admin = models.BooleanField(default=True)
+    email_subject_for_admin = models.CharField(
+        max_length=255,
+    )
+    email_content_for_admin = models.TextField(
+        help_text="Available variables: {first_name}, {last_name}, {phone_number}, {email}, {details}",
+    )
+    admin_recipient_email = models.EmailField()
+    send_email_to_visitor = models.BooleanField(default=True)
+    email_subject_for_visitor = models.CharField(
+        max_length=255,
+    )
+    email_content_for_visitor = models.TextField(
+        help_text="available_variables: {first_name}, {last_name}",
+    )
+
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel("send_email_to_admin"),
+                FieldPanel("email_subject_for_admin"),
+                FieldPanel("email_content_for_admin"),
+                FieldPanel("admin_recipient_email"),
+            ],
+            heading="Email settings for admin",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("send_email_to_visitor"),
+                FieldPanel("email_subject_for_visitor"),
+                FieldPanel("email_content_for_visitor"),
+            ],
+            heading="Email settings for visitor",
+        ),
+    ]
+
+
+@register_setting
+class NavigationSettings(BaseSiteSetting):
+    header_navigation_links = StreamField(
+        [
+            (
+                "page_link",
+                blocks.StructBlock(
+                    [
+                        ("title", blocks.CharBlock()),
+                        ("page", blocks.PageChooserBlock()),
+                        (
+                            "open_in_new_tab",
+                            blocks.BooleanBlock(default=False, required=False),
+                        ),
+                    ]
+                ),
+            ),
+            (
+                "external_link",
+                blocks.StructBlock(
+                    [
+                        ("title", blocks.CharBlock()),
+                        ("url", blocks.URLBlock()),
+                        (
+                            "open_in_new_tab",
+                            blocks.BooleanBlock(default=False, required=False),
+                        ),
+                    ]
+                ),
+            ),
+        ]
+    )
+    footer_navigation_links = StreamField(
+        [
+            (
+                "page_link",
+                blocks.StructBlock(
+                    [
+                        ("title", blocks.CharBlock()),
+                        ("page", blocks.PageChooserBlock()),
+                        (
+                            "open_in_new_tab",
+                            blocks.BooleanBlock(default=False, required=False),
+                        ),
+                    ]
+                ),
+            ),
+            (
+                "external_link",
+                blocks.StructBlock(
+                    [
+                        ("title", blocks.CharBlock()),
+                        ("url", blocks.URLBlock()),
+                        (
+                            "open_in_new_tab",
+                            blocks.BooleanBlock(default=False, required=False),
+                        ),
+                    ]
+                ),
+            ),
+        ]
+    )
